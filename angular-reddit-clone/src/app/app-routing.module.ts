a43de0e2425c0c0b9a7c4237c6eba14b0ce0e186
lava <lavanyaselvaraj1234@gmail.com>
2023-11-15 13:10:41 -0500

    added Initial backend code

--- angular-reddit-clone/src/app/app-routing.module.ts
+++ angular-reddit-clone/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
+  {path:'', component:HeaderComponent},
+  {path:'signup',component: SignupComponent },
 ];
 
 @NgModule({
